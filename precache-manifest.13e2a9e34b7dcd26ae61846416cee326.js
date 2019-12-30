6f58dfd86d6bf12a1f26fa5e7034ad6dea9a157f
shem bar <shembar100@gmail.com>
2019-12-30 19:45:49 +0200

    Updates

---
+++ precache-manifest.13e2a9e34b7dcd26ae61846416cee326.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de683af2c3d9d0559e739babc6a9c488",
+    "url": "/PagesExample/index.html"
+  },
+  {
+    "revision": "ae33afbdf1c10f2a0ea6",
+    "url": "/PagesExample/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7fe8ce5c07cd4bebe188",
+    "url": "/PagesExample/static/js/2.b27c7fee.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/PagesExample/static/js/2.b27c7fee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ae33afbdf1c10f2a0ea6",
+    "url": "/PagesExample/static/js/main.fb1bfff5.chunk.js"
+  },
+  {
+    "revision": "c17c71c987efba9c0e64",
+    "url": "/PagesExample/static/js/runtime-main.ba9ff6b0.js"
+  }
+]);
